5a72fbbf5881129731caaa74aa4d0042e8a586ae
John Bussey <jbussey365@yahoo.com>
2024-06-08 17:53:04 -0400

    parameterized tests passing

--- tests/test_board.py
+++ tests/test_board.py
@@ -1,15 +1,11 @@
+import pytest
 from models import board
 
-def test_get_start_points_flat():
-   returned = board.get_start_points((3,3))
-   correct = [(0,0), (0,1), (0,2), 
-              (1,0),        (1,2),
-              (2,0), (2,1), (2,2)]
-   assert returned == correct
-
-def test_get_start_points_3d():
-   returned = board.get_start_points((3,4,3))
-   correct = [(0,0,0), (0,0,1), (0,0,2), 
+get_start_point_inputs = [
+   ((3,3), [(0,0), (0,1), (0,2), 
+            (1,0),        (1,2),
+            (2,0), (2,1), (2,2)]),
+   ((3,4,3), [(0,0,0), (0,0,1), (0,0,2), 
               (0,1,0), (0,1,1), (0,1,2),
               (0,2,0), (0,2,1), (0,2,2),
               (0,3,0), (0,3,1), (0,3,2),
@@ -23,24 +19,23 @@ def test_get_start_points_3d():
               (2,1,0), (2,1,1), (2,1,2),
               (2,2,0), (2,2,1), (2,2,2),
               (2,3,0), (2,3,1), (2,3,2)
-              ]
-   assert returned == correct
+              ]),
+]
 
+@pytest.mark.parametrize("shape,expected", get_start_point_inputs)
+def test_get_start_points_flat(shape, expected):
+   returned = board.get_start_points(shape)
+   assert returned == expected
 
-def test_build_line_straight():
-   returned = board.build_line((3,3), (0,0), (0,1))
-   correct = [(0,0), (0,1), (0,2)]
-   assert returned == correct
-
-def test_build_line_2d_diag():
-   returned = board.build_line((3,3), (0,0), (1,1))
-   correct = [(0,0), (1,1), (2,2)]
-   assert returned == correct
-
-def test_build_line_3d_diag():
-   returned = board.build_line((3,3,4), (2,2,3), (-1,-1,-1))
-   correct = [(0,0,1), (1,1,2), (2,2,3)]
-   assert returned == correct
+build_line_inputs = [   
+   ((3,3), (0,0), (0,1), [(0,0), (0,1), (0,2)]),   
+   ((3,3), (0,0), (1,1), [(0,0), (1,1), (2,2)]),   
+   ((3,3,4), (2,2,3), (-1,-1,-1), [(0,0,1), (1,1,2), (2,2,3)]),         
+]
+@pytest.mark.parametrize("shape,start,diffs,expected", build_line_inputs)
+def test_build_line(shape, start, diffs, expected):
+   returned = board.build_line(shape, start, diffs)
+   assert returned == expected
 
 def test_build_all_lines_2d():
    returned = board.build_all_lines((3,3), 3)
