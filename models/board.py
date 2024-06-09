dd199fd47b00aefa0306069330f0ed9f1714d61e
John Bussey <jbussey365@yahoo.com>
2024-06-09 19:18:51 -0400

    renames and refactors

--- models/board.py
+++ models/board.py
@@ -3,22 +3,24 @@ import itertools
 from itertools import product
 import numpy as np
 
-class Board:
+class Game:
 
   board = []
   win_length: int
   players: List[str]
   gravity_dimension: int
+  lines: List[List[tuple[int]]]
   
   def __init__(self,
-               dimensions: List[int], 
+               shape: List[int], 
                win_length: int,
                players = ['X', 'O'],
                gravity_dimension=None) -> None:
-    self.board = np.full(dimensions, None)
+    self.board = np.full(shape, None)
     self.win_length = win_length
     self.players = players
     self.gravity_dimension = gravity_dimension
+    self.lines = build_all_lines(shape, win_length)
     
   def __str__(self) -> str:
     return str(self.board)
@@ -26,40 +28,6 @@ class Board:
   def add_move(self, move: List[int], player: int) -> None:
     self.board[*move] = player
 
-  def get_lines(self):
-    lines = []
-    dim_vals_full = [list(range(dim)) for dim in self.board.shape]
-    for zero_dim in range(self.board.ndim):
-      dim_vals = dim_vals_full.copy()
-      dim_vals[zero_dim] = [0]
-      all_inds = np.array(list(product(*dim_vals)))
-      for ind in all_inds:
-        line = [ind]
-        for i in range(1, self.board.shape[zero_dim]):
-          new_ind = ind.copy()
-          new_ind[zero_dim] += i
-          line.append(new_ind)
-        lines.append(line)
-    return np.array(lines)
-
-
-  
-  def check_line_win(self, line: List[int]) -> int|None:
-    if len(line) < self.win_length:
-      return False
-    current_player = None
-    tally = 0
-    for val in line:
-      if val is None:
-        current_player = None
-        tally = 0
-      elif val == current_player:
-        tally += 1
-        if tally >= self.win_length:
-          return current_player
-      else:
-        current_player = val
-        tally = 1
 
 
 
@@ -73,7 +41,7 @@ class Board:
 # print(len(lines))
 # print(lines)
 
-def middle_out(start: tuple[int], diff: tuple[int], shape: tuple[int]) -> bool:
+def is_middle_out(start: tuple[int], diff: tuple[int], shape: tuple[int]) -> bool:
   for i, val in enumerate(start):
     if val in [0, shape[i] - 1] and diff[i] != 0:
       return False
@@ -83,13 +51,13 @@ def build_all_lines(shape: tuple[int], win_length: int):
   lines = []
   starts = get_start_points(shape)
   diffs = get_diffs(shape)
-  for start in starts:
-    for diff in diffs:
-      if middle_out(start, diff, shape):
-        continue
-      line = build_line(shape, start, diff)
-      if len(line) >= win_length and line not in lines:
-        lines.append(line)
+  for(start, diff) in itertools.product(starts, diffs):
+    if is_middle_out(start, diff, shape):
+      continue
+    line = build_line(shape, start, diff)
+    if len(line) >= win_length and line not in lines:
+      lines.append(line)
+
   return sorted(lines)
 
 def get_start_points(shape: tuple[int]) -> List[tuple[int]]:
--- tests/test_board.py
+++ tests/test_board.py
@@ -21,7 +21,6 @@ get_start_point_inputs = [
               (2,3,0), (2,3,1), (2,3,2)
               ]),
 ]
-
 @pytest.mark.parametrize("shape,expected", get_start_point_inputs)
 def test_get_start_points(shape, expected):
    returned = board.get_start_points(shape)
@@ -37,7 +36,7 @@ def test_build_line(shape, start, diffs, expected):
    returned = board.build_line(shape, start, diffs)
    assert returned == expected
 
-build_all_line_inputs = [ 
+build_all_lines_output_inputs = [ 
    ((2,2,2), 2, [    
       [(0, 0, 0), (0, 0, 1)], 
       [(0, 0, 0), (0, 1, 0)], 
@@ -90,7 +89,19 @@ build_all_line_inputs = [
       [(2,0), (2,1)],
    ])
 ]
-@pytest.mark.parametrize("shape,win_length,expected", build_all_line_inputs)
-def test_build_all_lines(shape, win_length, expected):
+@pytest.mark.parametrize("shape,win_length,expected", build_all_lines_output_inputs)
+def test_build_all_lines_output(shape, win_length, expected):
    returned = board.build_all_lines(shape, win_length)
    assert returned == expected
+
+build_all_lines_len_inputs = [
+   ((2,2,2), 2, 28),
+   ((3,3), 3, 8),
+   ((3,2), 2, 9),
+   ((4,4,4), 4, 76),
+   ((4,4,3), 3, 106),
+]
+@pytest.mark.parametrize("shape,win_length,expected", build_all_lines_len_inputs)
+def test_build_all_lines_len(shape, win_length, expected):
+   returned = board.build_all_lines(shape, win_length)
+   assert len(returned) == expected
